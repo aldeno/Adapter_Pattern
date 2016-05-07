a3638a8ba95848f9cbcba56e8818a7c8dde93fcb
nekoneki <alden.obradovic@live.com>
2016-05-07 11:37:14 +0200

    Add project files.

---
+++ Adapter_Pattern/Interfaces/IAttacker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter_Pattern.Interfaces
+{
+    public interface IAttacker
+    {
+        void AssignDriver(string driverName);
+
+        void DriveForward();
+        void DriveBackward();
+        void DriveForward();
+        void DriveForward();
+    }
+}
